3682c1d39da3b24bbb82e1a71fe81d77c6d5f00f
noobstick <tammai@buffalo.edu>
2016-12-05 21:59:50 -0500

    added ui-routing

---
+++ MyWebGallery/app/app.config.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app', ['ui.router'])
+        .config(function ($stateProvider) {
+            var adminState = {
+                name: 'admin',
+                url: '/admin',
+                template: '<h3>Admin baby</h3>'
+            }
+
+            var consumerState = {
+                name: 'consumer',
+                url: '/consumer',
+                template: '<h3>consumer baby</h3>'
+            }
+            $stateProvider.state(adminState);
+            $stateProvider.state(consumerState);
+
+        });
+})();
